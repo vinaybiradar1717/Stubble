9a041cfcceff32c78dd653b11e17229e89f30410
sarja830 <sarthjain830@gmail.com>
2020-04-26 17:03:33 +0530

    kfold

---
+++ sarthak/kfold_svr_stubble.py
@@ -0,0 +1,45 @@
+# k-Fold Cross Validation
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset =pd.read_csv('ALLHDPD.csv')
+dataset =dataset.bfill()
+X=dataset.iloc[:,[1,3,4,5,6]].values
+y=dataset.iloc[:,12:13].values
+
+
+# Splitting the dataset into the Training set and Test set
+
+from sklearn.model_selection import train_test_split 
+X_train,X_test,y_train,y_test =train_test_split(X,y,test_size=.2,random_state=0)
+
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler 
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+#X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+# Fitting the svr to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X_train,y_train)
+
+# Predicting a new result
+
+y_pred=sc_y.inverse_transform(regressor.predict(sc_X.transform(X_test)))
+
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = regressor, X = X_train, y = y_train, cv = 10)
+accuracies.mean()    #mean of accuracies 
+accuracies.std()    #Standard deviation foraccuracies
+
---
+++ sarthak/stubble_pca.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# PCA
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset =pd.read_csv('ALLHDPD.csv')
+dataset =dataset.bfill()
+X=dataset.iloc[:,[1,3,4,5,6]].values
+y=dataset.iloc[:,12:13].values
+
+
+# Splitting the dataset into the Training set and Test set
+
+from sklearn.model_selection import train_test_split 
+X_train,X_test,y_train,y_test =train_test_split(X,y,test_size=.2,random_state=0)
+
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler 
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+#X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+
+# Applying PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+
+
+# Fitting the svr to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X_train,y_train)
+
+# Predicting a new result
+
+y_pred=sc_y.inverse_transform(regressor.predict(sc_X.transform(X_test)))
